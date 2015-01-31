334f5f1e2fc898237c07546019b6043230192d83
Oscar <od_cortez@galileo.edu>
2015-01-31 14:50:12 -0600

    changes in songs.html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
-
-Parse.initialize("YJwHjwYF08scjC2SEdl9Ve2YFB3056FQfkUgGu8p", "awDPDR2YNfZNtbJ54VzEZPiWJZmLekU8qkJxnlOw");
-
-angular.module('AuthApp', [])
-.run(['$rootScope', function($scope) {
+Parse.initialize("o738tDIjX7Oq1jSB1PtSG6LfVeZqOgpaKH0pK3dt", "p7JfKdqPlYwWoenFcH1pnxR73YDzNaHAjz6iAwhq");
+//Starting angular and setting routes
+var angularTesting = angular.module('angularTesting', ["ngRoute"]).config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'inicio.html'
+  })
+  .when('/songs', {
+    controller: 'jsonData',
+    templateUrl: 'songs.html'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+}).run(['$rootScope', "$location", function($scope, $location) {
   $scope.scenario = 'Sign up';
   $scope.currentUser = Parse.User.current();
-  
-  $scope.signUp = function(form) {
+  $scope.signUp = function(form, newPath) {
     var user = new Parse.User();
     user.set("email", form.email);
     user.set("username", form.username);
@@ -15,18 +24,20 @@ angular.module('AuthApp', [])
     user.signUp(null, {
       success: function(user) {
         $scope.currentUser = user;
+        $location.path(newPath);
         $scope.$apply();
       },
       error: function(user, error) {
         alert("Unable to sign up:  " + error.code + " " + error.message);
       }
-    });    
+    });
   };
   
-  $scope.logIn = function(form) {
+  $scope.logIn = function(form, newPath) {
     Parse.User.logIn(form.username, form.password, {
       success: function(user) {
         $scope.currentUser = user;
+        $location.path(newPath);
         $scope.$apply();
       },
       error: function(user, error) {
@@ -38,5 +49,23 @@ angular.module('AuthApp', [])
   $scope.logOut = function(form) {
     Parse.User.logOut();
     $scope.currentUser = null;
+    $location.path("/index");
   };
 }]);
+angularTesting .filter("pictureSong", function(){
+  return function(item){
+    return (JSON.stringify(item)).slice(10,-18);
+  };
+});
+
+angularTesting .controller('SearchController', function ($scope, $http) {
+  $http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=85b8c37b1a6be5182a5ed0549c4a7400&format=json').success(function(data) {
+    $scope.phones = data.tracks.track;   
+
+ });
+
+$scope.orderBy = function(orden){
+  $scope.orderSelect = orden;
+};
+  
+});
